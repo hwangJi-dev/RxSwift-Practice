c70e3fcdfdc4b435ffd9263fc0e4a8e3f031ca40
hwangJi-dev <melon2228@swu.ac.kr>
2021-07-19 23:56:52 +0900

    init

--- rxSwiftPractice/rxSwiftPractice/ViewController.swift
+++ rxSwiftPractice/rxSwiftPractice/ViewController.swift
@@ -11,6 +11,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let oneTwoThree = 1...3
+        for number in oneTwoThree {
+          print(number)
+        }
+        let stringSequence = Observable.just("이것은 문자열 yo입니다")
+        let oddSequence = Observable.from([1, 3, 5, 7, 9])
+        let dictSequence = Observable.from([1:"Rx",2:" 빠른"])
         // Do any additional setup after loading the view.
     }
 
